01b8fe7845f7ead4fdaac6cd1bf2e7195d5a96dd
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-31 12:00:09 +0530

    Updated class and object

--- src-typescript/demo4-employee.ts
+++ src-typescript/demo4-employee.ts
@@ -1,28 +1,38 @@
-export class Employee
-{
-    public id:number;
-    public name:string;
-    public salary:number;
-    public performance:string;
-    public companyName:string;
+export class Employee {
+    public id: number;
+    public name: string;
+    public salary: number;
+    public performance: string;
+    public static companyName: string;
 
-    constructor(id:number,name:string,salary:number,performance:string,companyName:string)
-    {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
-        this.performance=performance;
-        this.companyName=companyName;
+    constructor(id: number, name: string, salary: number, performance: string) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.performance = performance;
+    }
+
+    //non-static method
+    public displayEmployeeRecord(): void {
+        console.log("Employee Id: " + this.id)
+        console.log("Employee Name: " + this.name)
+        console.log("Employee Salary: " + this.salary)
+        console.log("Employee Performance: " + this.performance)
+        console.log("Company Name: " + Employee.companyName)
+        console.log("---------------------------------------------------------")
     }
 }
 
 
-let emp1=new Employee(101,"John",8000,"Good","EY")
-let emp2=new Employee(102,"Kim",9000,"Average", "EY")
-//create third object
+Employee.companyName = "EY"
 
-console.log(emp1.id)
-console.log(emp1.name)
+let emp1 = new Employee(-101, "John", 8000, "Good")
+let emp2 = new Employee(102, "Kim", 9000, "Average")
+let emp3: Employee = new Employee(103, "Saul", 4000, "Bad")
 
-console.log(emp2.id)
-console.log(emp2.name)
+
+emp2.displayEmployeeRecord();
+
+emp1.displayEmployeeRecord();
+
+emp3.displayEmployeeRecord();
