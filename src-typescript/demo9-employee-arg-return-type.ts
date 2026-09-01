d19e44179436b7bf13d3a93416981e368d9975b2
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-01 11:12:40 +0530

    User defined datatypes as a method argument

---
+++ src-typescript/demo9-employee-arg-return-type.ts
@@ -0,0 +1,52 @@
+export class Employee {
+    public id: number;
+    public name: string;
+    public salary: number;
+    public performance: string;
+    public static companyName: string;
+
+    constructor(id: number, name: string, salary: number, performance: string) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.performance = performance;
+    }
+
+    //non-static method
+    public displayEmployeeRecord(): void {
+        console.log("Employee Id: " + this.id)
+        console.log("Employee Name: " + this.name)
+        console.log("Employee Salary: " + this.salary)
+        console.log("Employee Performance: " + this.performance)
+        console.log("Company Name: " + Employee.companyName)
+        console.log("---------------------------------------------------------")
+    }
+
+
+    public static printEmployeeRecord(x: Employee): void {
+        console.log("Employee Id: " + x.id)
+        console.log("Employee Name: " + x.name)
+        console.log("Employee Salary: " + x.salary)
+        console.log("Employee Performance: " + x.performance)
+        console.log("Company Name: " + Employee.companyName)
+        console.log("---------------------------------------------------------")
+    }
+
+
+}
+
+
+Employee.companyName = "EY"
+
+let emp1 = new Employee(-101, "John", 8000, "Good")
+let emp2 = new Employee(102, "Kim", 9000, "Average")
+let emp3: Employee = new Employee(103, "Saul", 4000, "Bad")
+
+
+emp2.displayEmployeeRecord();
+emp1.displayEmployeeRecord();
+emp3.displayEmployeeRecord();
+
+Employee.printEmployeeRecord(emp2);
+Employee.printEmployeeRecord(emp1);
+Employee.printEmployeeRecord(emp3);
