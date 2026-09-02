7962bf55d1bb2390b0fd021ad433aacd6e49584c
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-02 13:33:15 +0530

    Updated session notes

--- src-web/demo2-orange-hrm.ts
+++ src-web/demo2-orange-hrm.ts
@@ -6,6 +6,8 @@ let page = await context.newPage()
 
 await page.goto("https://orangehrm.com/book-a-free-demo")
 
+await page.locator("xpath=//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").click()
+
 await page.locator("xpath=//input[@id='Form_getForm_FullName']").fill("John Wick")
 await page.locator("xpath=//input[@id='Form_getForm_Email']").fill("john@gmail.com")
 // enter phone number - 99898
@@ -13,11 +15,14 @@ await page.locator("xpath=//input[@id='Form_getForm_Email']").fill("john@gmail.c
 // enter job title - QA 
 
 // select country as India 
-await page.locator("xpath=//select[@id='Form_getForm_Country']").selectOption({label:"India"})
+// await page.locator("xpath=//select[@id='Form_getForm_Country']").selectOption({label:"India"})
 // select employee count - 51 - 200 
 
 //click on Get a Free Demo
+await page.locator("xpath=//select[@id='Form_getForm_Country']").selectOption({ label: "India" })
+await page.locator("xpath=//select[@id='Form_getForm_NoOfEmployees']").selectOption({ label: "51 - 200" })
 
+await page.locator("xpath=//input[@id='Form_getForm_action_submitForm']").click()
 
-await page.waitForTimeout(5000)
+await page.waitForTimeout(8000)
 await browser.close()
