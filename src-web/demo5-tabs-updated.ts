0ca9ecd66b9b0da95750c1e0fb3ba043a249a642
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-03 12:30:10 +0530

    Using Promise.all for multiple tabs

--- src-web/demo5-tabs-updated.ts
+++ src-web/demo5-tabs-updated.ts
@@ -7,19 +7,13 @@ let page = await context.newPage();
 await page.goto("https://opensource-demo.orangehrmlive.com/");
 
 
-//Start waiting for new page (tab) before clicking. Note no await.
-const promisePage=page.waitForEvent("popup");
+// const [title1,title2,title3]=await Promise.all([page.title(),page.title(),page.title()])
 
-//click on element that opens a new tab
-await page.locator("xpath=//a[text()='OrangeHRM, Inc']").click();
+const [newPage,]=await Promise.all([page.waitForEvent("popup"),page.locator("xpath=//a[text()='OrangeHRM, Inc']").click()])
 
-//await on the called waitforEvent method to complete. it will return new tab detail - Page 
-const newPage=await promisePage;
-
-//newPage --> points to newly opened tab
 await newPage.locator("xpath=//button[text()='Book a Free Demo']").click();
 await newPage.locator("xpath=//input[@id='Form_getForm_FullName']").fill("John Wick")
-// enter phone number as 92939329
+// // enter phone number as 92939329
 
 await page.waitForTimeout(5000);
 await browser.close();
