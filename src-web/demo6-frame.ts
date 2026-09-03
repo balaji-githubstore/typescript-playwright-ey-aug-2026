2c034018d680f5608121d90d6d617718c9032555
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-03 13:20:14 +0530

    Frame handling  - Complete the code

--- src-web/demo6-frame.ts
+++ src-web/demo6-frame.ts
@@ -6,9 +6,18 @@ let page = await context.newPage();
 //page --> points to 1st tab
 await page.goto("https://app.thetestingacademy.com/playwright/frames/");
 
+
+const vehicleFrame=page.frameLocator("xpath=//iframe[@name='vehicle-form']");
+
 //enter vehicle name as creta
+await vehicleFrame.locator("xpath=//input[@name='vehicleName']").fill("Creta");
+await vehicleFrame.locator("xpath=//input[@name='ownerName']").fill("John");
 
-
+//registration number TN 13 AY6767
+//vechicle type as SUV
+// year - 2020
+// notes - 6 year old
+//click on submit 
 
 
 await page.waitForTimeout(5000);
