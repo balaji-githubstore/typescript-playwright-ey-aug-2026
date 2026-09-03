1d8b5a62344758ddc43375505e761a9d6137959b
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-03 12:16:51 +0530

    Added Code for Multiple tabs/windows

--- src-web/demo1-launch-browser.ts
+++ src-web/demo1-launch-browser.ts
@@ -7,13 +7,20 @@ let browser=await chromium.launch({channel:"chrome",headless:false})
 let context=await browser.newContext()
 
 // getting FirstTab - Page instance 
+//page --> tab 1
 let page=await context.newPage()
 
-await page.goto("https://www.google.com/")
+//page1 --> tab 2
+let page1=await context.newPage()
 
+
+await page.goto("https://www.google.com/")
+await page1.goto("https://facebook.com")
 let actualTitle:string= await page.title()
 console.log(actualTitle)
 
+
+let page2 = await context.newPage()
 
 await page.waitForTimeout(5000)
 await browser.close()
--- src-web/demo4-tabs.ts
+++ src-web/demo4-tabs.ts
@@ -1,26 +1,28 @@
 import { chromium } from "playwright"
 
-let browser = await chromium.launch({ channel: "chrome", headless: false })
-let context = await browser.newContext()
-let page = await context.newPage()
-
-await page.goto("https://opensource-demo.orangehrmlive.com/")
-
-
-// click on OrangeHRM, Inc
+let browser = await chromium.launch({ channel: "chrome", headless: false });
+let context = await browser.newContext();
+let page = await context.newPage();
+//page --> points to 1st tab
+await page.goto("https://opensource-demo.orangehrmlive.com/");
 
 
+//Start waiting for new page (tab) before clicking. Note no await.
+const promisePage=page.waitForEvent("popup");
 
-//below work I will do it 
-// click on Book a Free Demo
+//click on element that opens a new tab
+await page.locator("xpath=//a[text()='OrangeHRM, Inc']").click();
+
+//await on the called waitforEvent method to complete. it will return new tab detail - Page 
+const newPage=await promisePage;
+
+//newPage --> points to newly opened tab
+await newPage.locator("xpath=//button[text()='Book a Free Demo']").click();
+await newPage.locator("xpath=//input[@id='Form_getForm_FullName']").fill("John Wick")
+// enter phone number as 92939329
 
 
 
+await page.waitForTimeout(5000);
+await browser.close();
 
-
-
-
-
-
-await page.waitForTimeout(5000)
-await browser.close()
