e0f753d0bd4630883e6399f3f20f6379386a8193
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-04 10:49:02 +0530

    Handled javascript alert

--- src-web/demo7-alert.ts
+++ src-web/demo7-alert.ts
@@ -3,12 +3,21 @@ import { chromium } from "playwright"
 let browser = await chromium.launch({ channel: "msedge", headless: false });
 let context = await browser.newContext();
 let page = await context.newPage();
+await page.waitForTimeout(5000);
 //page --> points to 1st tab
 await page.goto("https://www.nasscom.in/nasscom-membership");
 
+// register the dialog event to handle alert on your own
+page.on("dialog",async dialog=>{
+     let actualAlertMessage= dialog.message();
+     console.log(actualAlertMessage);
+     await dialog.accept();
+})
 
 // click on Calculate fee 
-
+// javascript alerts or dialog are auto-handled by playwright. 
+// You can modify the existing behaviour of playwright alert handling using page.on("dialog") event. 
+await page.locator("xpath=//a[text()='Calculate Fee']").click();
 
 await page.waitForTimeout(5000);
 await browser.close();
