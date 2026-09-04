b2b42375cbbd19232dc952ade8b0e0b15e1c709d
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-04 12:14:14 +0530

    Upload option 3 - page.on("filechooser")

---
+++ src-web/demo8-upload-option3.ts
@@ -0,0 +1,18 @@
+import { chromium } from "playwright"
+
+let browser = await chromium.launch({ channel: "msedge", headless: false });
+let context = await browser.newContext();
+let page = await context.newPage();
+await page.waitForTimeout(5000);
+
+await page.goto("https://www.ilovepdf.com/pdf_to_word");
+
+page.on("filechooser",async fileChooser=>{
+    await fileChooser.setFiles("D:\\Mine\\Balaji Dinakaran Trainer Profile AI 2026.pdf")
+});
+
+await page.locator("xpath=//span[text()='Select PDF file']").click();
+
+
+await page.waitForTimeout(5000);
+await browser.close();
