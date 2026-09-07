ebcbc06d5b0008cc668e302fd77f22bfc1b7f18f
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-07 11:48:15 +0530

    Working on collection of locators

---
+++ src-web/demo11-locators.ts
@@ -0,0 +1,35 @@
+import { chromium } from "playwright"
+
+let browser = await chromium.launch({ channel: "chrome", headless: false })
+let context = await browser.newContext()
+let page = await context.newPage()
+
+await page.goto("https://google.com")
+
+// await page.locator("xpath=//a").nth(0).click()
+
+let linksLocators = await page.locator("xpath=//a")
+
+console.log(await linksLocators.count())
+
+
+for (let i = 0; i < await linksLocators.count(); i++) {
+
+    await linksLocators.nth(i).highlight()
+    let actualText = await linksLocators.nth(i).innerText();
+    console.log(actualText)
+
+    let hrefAttValue = await linksLocators.nth(i).getAttribute("href")
+    console.log(hrefAttValue)
+
+    console.log("--------------------")
+}
+
+
+let allLinkText=await page.locator("xpath=//a").allInnerTexts()
+console.log(allLinkText)
+
+
+await page.waitForTimeout(5000);
+await browser.close();
+
