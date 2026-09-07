3a939b4fb82299d6ada2435d61e9539d3296bb38
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-07 12:06:44 +0530

    Print all the name from the table of page 1

--- src-web/demo12-tables.ts
+++ src-web/demo12-tables.ts
@@ -6,7 +6,11 @@ let page = await context.newPage()
 
 await page.goto("https://datatables.net/examples/select/checkbox/checkbox.html")
 
+
+//write a for loop and then print all name (1 to 10)
 //get the firstname from the table and print it 
+let name1=await page.locator("xpath=//table[@id='example']/tbody/tr[2]/td[2]").innerText();
+console.log(name1)
 
 
 await page.waitForTimeout(5000);
